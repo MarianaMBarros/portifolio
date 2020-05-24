0e8b1f33ac8935581428a415f166c395183b97fe
Mariana Barros <mamirandabarros11@gmail.com>
2020-05-24 12:49:25 -0300

    atualizacao do css e js

--- src/main.js
+++ src/main.js
@@ -1,20 +1,36 @@
-import portifolios from './portifolio/portifolio.js';
+import perfil from './perfil/perfil.js';
 
+function conhecimento() {
+  const html = perfil.habilidades.reduce((data, value) => {
+    return data += `<li><i class="fab ${value.icone} fa-2x">${value.tecnologia}</i></li>`
+  }, "");
+  document.getElementById("list").innerHTML = html
+}
+conhecimento();
 
 function mostarPortifolio() {
-  const html = portifolios.reduce((data, value) => {
+  const html = perfil.portifolios.reduce((data, value) => {
     return data += `<div class="portifolio-item pink">
     <h3>${value.projeto}</h3>
     <div>
-      <a href="${value.link}" target="_blank"><img src="${value.img}"
-          alt="${value.projeto}"></a>
+      <a href="${value.link}" target="_blank"><img src="${value.img}" alt="${value.projeto}"></a>
     </div>
-    <span>${value.descricao}</span>
+    <p>${value.descricao}</p>
   </div>`
-  }, "")
+  }, "");
   document.getElementById("portifolio-container").innerHTML = html;
 }
-mostarPortifolio()
+mostarPortifolio();
+
+
+function contato() {
+  const html = perfil.contatos.reduce((data, value) => {
+    return data += `<li><a href=${value.link} target="_blank"><i class="fab ${value.icone} fa-3x"></i></a></li>`
+  }, "");
+  document.getElementById("contato-icone").innerHTML = html;
+}
+contato();
+
 
 
 $('.portifolio-container').slick({
---
+++ src/perfil/perfil.js
@@ -0,0 +1,89 @@
+export default {
+  portifolios: [
+    {
+      "projeto": "Cifra do Amor",
+      "img": "./img/cifra.jpg",
+      "link": "https://marianambarros.github.io/SAP004-cipher/",
+      "descricao": "O projeto tem como finalidade cifrar e decifrar mensagens de amor para casais",
+    },
+    {
+      "projeto": "Pokediary",
+      "img": "./img/pokemon.jpg",
+      "link": "https://marianambarros.github.io/SAP004-data-lovers/",
+      "descricao": "Função do Projeto é exibir e informar dados dos pokémons 1° geração",
+    },
+    {
+      "projeto": "Cifra do Amor",
+      "img": "./img/cifra.jpg",
+      "link": "https://marianambarros.github.io/SAP004-cipher/",
+      "descricao": "O projeto tem como finalidade cifrar e decifrar mensagens de amor para casais",
+    },
+    {
+      "projeto": "Pokediary",
+      "img": "./img/pokemon.jpg",
+      "link": "https://marianambarros.github.io/SAP004-data-lovers/",
+      "descricao": "Função do Projeto é exibir e informar dados dos pokémons 1° geração",
+    },
+    {
+      "projeto": "Cifra do Amor",
+      "img": "./img/cifra.jpg",
+      "link": "https://marianambarros.github.io/SAP004-cipher/",
+      "descricao": "O projeto tem como finalidade cifrar e decifrar mensagens de amor para casais",
+    },
+    {
+      "projeto": "Pokediary",
+      "img": "./img/pokemon.jpg",
+      "link": "https://marianambarros.github.io/SAP004-data-lovers/",
+      "descricao": "Função do Projeto é exibir e informar dados dos pokémons 1° geração",
+    },
+
+  ],
+  habilidades: [
+    {
+      tecnologia: "JavaScript",
+      icone: "fa-js",
+    },
+    {
+      tecnologia: "Java",
+      icone: "fa-java ",
+    },
+    {
+      tecnologia: "CSS",
+      icone: "fa-css3",
+    },
+    {
+      tecnologia: "React",
+      icone: "fa-react",
+    },
+    {
+      tecnologia: "Html",
+      icone: "fa-html5",
+    },
+    {
+      tecnologia: "Scrum",
+      icone: "fa-tasks",
+    },
+    {
+      tecnologia: "Git",
+      icone: "fa-git-alt",
+    },
+    {
+      tecnologia: "C#",
+      icone: "./img/csharp.jpg",
+    },
+    {
+      tecnologia: "SQL Server",
+      icone: "/src/img/sql.jpg",
+    },
+  ],
+  contatos: [
+    {
+      link: "https://github.com/MarianaMBarros",
+      icone: "fa-github",
+    },
+    {
+      link: "https://www.linkedin.com/in/marianambarros",
+      icone: "fa-linkedin",
+    },
+  ]
+}
